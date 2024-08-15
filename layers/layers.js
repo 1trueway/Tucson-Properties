5b268a8ee866bf642f5837967b9c3248a2a07f69
1trueway <michael.miller.true@gmail.com>
2024-08-15 14:15:17 -0700

    fix

--- layers/layers.js
+++ layers/layers.js
@@ -38,7 +38,7 @@ var lyr_25mAbove_2 = new ol.layer.VectorTile({
 	source: new ol.source.VectorTile({
 		attributions: ' ',
 		format: new ol.format.MVT(),
-		url: 'file:///Vectors/{z}/{x}/{y}.pbf'
+		url: 'Vectors/{z}/{x}/{y}.pbf'
 	})
 });
 var group_Vectors = new ol.layer.Group({
